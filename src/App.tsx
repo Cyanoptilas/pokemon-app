158cae565df8ca0ad26fbcae2276318d34d9673c
Cyanoptilas <75958367+Cyanoptilas@users.noreply.github.com>
2024-03-12 23:09:47 +0900

    ADD fetch関数追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import "./App.css";
+import { getAllPokemonData } from "./utils/pokemon";
 
 function App() {
+  const initialUrl: string = "https://pokeapi.co/api/v2/pokemon/ditto";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      const res = await getAllPokemonData(initialUrl);
+      console.log("🚀 ~ fetchPokemonData ~ res:", res);
+    };
+    fetchPokemonData();
+  }, []);
+
   return (
     <>
-      <div></div>
+      <div>{initialUrl}</div>
     </>
   );
 }
---
+++ src/utils/pokemon.ts
@@ -0,0 +1,8 @@
+export const getAllPokemonData = (url: string) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data))
+      .catch((err) => reject(err));
+  });
+};
